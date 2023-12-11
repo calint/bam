0ff535a58dcf5a8ca6b7caadc2d544be78c60a0b
calint <calin.tenitchi@gmail.com>
2023-12-11 20:09:36 +0100

    .

--- src/game/objects/ufo2.hpp
+++ src/game/objects/ufo2.hpp
@@ -3,6 +3,7 @@
 #include "../../engine.hpp"
 // then objects
 #include "game_object.hpp"
+#include "ship2.hpp"
 #include "utils.hpp"
 
 class ufo2 final : public game_object {
@@ -17,6 +18,18 @@ public:
   }
 
   void pre_render() override { sprs.pre_render(this); }
+
+  auto on_collision(game_object *obj) -> bool override {
+    ship2 *shp = new (objects.allocate_instance()) ship2{};
+    shp->x = obj->x;
+    shp->y = obj->y - sprite_height;
+    shp->dx = float(random(-100, 100));
+    shp->ddx = -shp->dx * 0.25f;
+    shp->dy = -100;
+    shp->ddy = 100;
+
+    return game_object::on_collision(obj);
+  }
 
   void on_death_by_collision() override {
     create_fragments(x, y, 32, 150, 1000);
