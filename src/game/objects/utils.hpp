99785e9f3157b1c0de83006a7e246826f474b32d
calint <calin.tenitchi@gmail.com>
2023-12-12 12:42:08 +0100

    .

--- src/game/objects/utils.hpp
+++ src/game/objects/utils.hpp
@@ -4,17 +4,17 @@
 // then objects
 #include "fragment.hpp"
 
-static float random_float(float min, float max) {
+static float random_float(const float min, const float max) {
   constexpr float rand_max_inv = 1.0f / float(RAND_MAX);
   return (max - min) * float(rand()) * rand_max_inv + min;
 }
 
-static float display_x_for_touch(int16_t x) {
+static float display_x_for_touch(const int16_t x) {
   static constexpr float fact_x = float(display_width) / touch_screen_range_x;
   return float(x - touch_screen_min_x) * fact_x;
 }
 
-static float display_y_for_touch(int16_t y) {
+static float display_y_for_touch(const int16_t y) {
   static constexpr float fact_y = float(display_height) / touch_screen_range_y;
   return float(y - touch_screen_min_y) * fact_y;
 }
@@ -24,7 +24,7 @@ class sprites_2x2 final {
   sprite *sprs[3];
 
 public:
-  sprites_2x2(game_object *obj, int top_left_index_in_16_sprites_row) {
+  sprites_2x2(game_object *obj, const int top_left_index_in_16_sprites_row) {
     obj->spr = sprites.allocate_instance();
     obj->spr->obj = obj;
     // additional 3 sprites
@@ -57,16 +57,17 @@ public:
   }
 };
 
-static void create_fragments(float orig_x, float orig_y, int frag_count,
-                             float frag_speed, unsigned long life_time_ms) {
-  for (int i = 0; i < frag_count; i++) {
+static void create_fragments(const float orig_x, const float orig_y,
+                             const int count, const float speed,
+                             const clk::time life_time_ms) {
+  for (int i = 0; i < count; i++) {
     fragment *frg = new (objects.allocate_instance()) fragment{};
     frg->die_at_ms = clk.ms + life_time_ms;
     frg->x = orig_x;
     frg->y = orig_y;
-    frg->dx = random_float(-frag_speed, frag_speed);
-    frg->dy = random_float(-frag_speed, frag_speed);
-    frg->ddx = 2 * random_float(-frag_speed, frag_speed);
-    frg->ddy = 2 * random_float(-frag_speed, frag_speed);
+    frg->dx = random_float(-speed, speed);
+    frg->dy = random_float(-speed, speed);
+    frg->ddx = 2 * random_float(-speed, speed);
+    frg->ddy = 2 * random_float(-speed, speed);
   }
 }
